7044e06024a71a747b869c7747b5919395e064e6
hugo <hugowen20@gmail.com>
2020-09-03 20:52:08 +0800

    108

---
+++ 108.go
@@ -0,0 +1,34 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func sortedArrayToBST(nums []int) *TreeNode {
+    if len(nums) == 0 {
+        return nil
+    }
+
+    var buildTree func(int, int) *TreeNode
+    buildTree = func(left int, right int) *TreeNode {
+        if left > right {
+            return nil
+        }
+        node := &TreeNode{
+            Val: 0,
+            Left: nil,
+            Right: nil,
+        }
+        mid := (left + right) / 2
+        node.Val = nums[mid]
+        node.Left = buildTree(left, mid - 1)
+        node.Right = buildTree(mid + 1, right)
+        return node
+    }
+
+    return buildTree(0, len(nums) - 1)
+}
